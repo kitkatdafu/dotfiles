0cb26310a87b7f5ac0b91b28231e7be99e48c7cf
Reid Chen <reid@deepneural.network>
2024-12-27 06:53:07 -0600

    update init file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,5 +1,17 @@
-;;;; use-package ;;;;
+(setq gc-cons-thresold 50000000)
+(setq large-file-warning-thresold 100000000)
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(fset 'yes-or-no-p 'y-or-n-p)
+(global-auto-revert-mode t)
+(add-hook 'before-save-hook 'whitespace-cleanup)
+(setq user-full-name "Reid Chen"
+      user-mail-address "reid@deepneural.network")
 
+
+;;;; use-package ;;;;
 (require 'package) ;; Emacs builtin
 
 ;; set package.el repositories
@@ -28,9 +40,50 @@
     (package-install package)))
 
 ;;;; UI ;;;;
-
 (scroll-bar-mode -1)
-(set-frame-font "PragmataPro Mono Liga 16" nil t)
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+(global-hl-line-mode +1)
+(line-number-mode +1)
+(global-display-line-numbers-mode +1)
+(column-number-mode t)
+(size-indication-mode t)
+(setq inhibit-startup-screen t)
+(setq frame-title-format
+      '((:eval (if (buffer-file-name)
+       (abbreviate-file-name (buffer-file-name))
+       "%b"))))
+(setq scroll-margin 0
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
+(global-prettify-symbols-mode t)
+(set-frame-font "PragmataPro Mono Liga 14" nil t)
+
+(use-package diff-hl
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
+  (add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode))
+
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-ayu-light t)
+  (doom-themes-visual-bell-config))
+
+(use-package doom-modeline
+  :ensure t
+  :init
+  (doom-modeline-mode 1))
+
+;;;; Org mode ;;;;
+(setq org-ellipsis "⤵")
+(use-package org-bullets :ensure t)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(setq org-hide-leading-stars t)
+(setq org-src-fontify-natively t)
+(setq org-src-window-setup 'current-window)
 
 ;;;; rust-mode ;;;;
 (use-package rust-mode
@@ -50,3 +103,98 @@
   :ensure t
   :config
   (add-hook 'after-init-hook 'global-company-mode))
+
+;;;; editing ;;;;
+(use-package smartparens
+  :ensure t
+  :diminish smartparens-mode
+  :config
+  (progn
+    (require 'smartparens-config)
+    (smartparens-global-mode 1)
+    (show-paren-mode t)))
+
+(use-package expand-region
+  :ensure t
+  :bind ("M-m" . er/expand-region))
+
+(use-package yasnippet
+  :ensure t
+  :config (use-package yasnippet-snippets :ensure t) (yas-reload-all))
+(yas-global-mode 1)
+
+(setq-default tab-width 4
+              indent-tabs-mode nil)
+
+(use-package let-alist)
+(use-package flycheck
+  :ensure t)
+
+(use-package magit
+  :ensure t
+  :bind
+  ("C-x g" . magit-status)
+  :config
+  (setq git-commit-summary-max-length 50)
+)
+
+(use-package golden-ratio :ensure t)
+(golden-ratio-mode 1)
+(use-package swiper
+  :ensure t)
+
+(use-package swiper :ensure t :config (progn (global-set-key "\C-s" 'swiper)))
+
+;; Enable vertico
+(use-package vertico
+  :ensure t
+  :custom
+  (vertico-resize t) ;; Grow and shrink the Vertico minibuffer
+  (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
+  :init
+  (vertico-mode))
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+;; A few more useful configurations...
+(use-package emacs
+  :custom
+  ;; Support opening new minibuffers from inside existing minibuffers.
+  (enable-recursive-minibuffers t)
+  ;; Hide commands in M-x which do not work in the current mode.  Vertico
+  ;; commands are hidden in normal buffers. This setting is useful beyond
+  ;; Vertico.
+  (read-extended-command-predicate #'command-completion-default-include-p)
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+                  (replace-regexp-in-string
+                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                   crm-separator)
+                  (car args))
+          (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(magit vertico flycheck diff-hl helm helm-projectile org-bullets doom-modeline doom-themes company rust-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
